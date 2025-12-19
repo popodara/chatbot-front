0c4c3297c8892259abd63a76b3c1add46f53154e
popodara <thierno.mamadoury@gmail.com>
2025-12-19 19:39:20 +0000

    update api url

--- config.tsx
+++ config.tsx
@@ -1,9 +1,6 @@
 import axiosClient from "axios";
-// TEST
- export const API_ROUTE = "http://localhost:9141";
 
-
-export const API_URL = API_ROUTE;
+export const API_URL = process.env.REACT_APP_API_URL;
 export const axios = axiosClient.create({
   baseURL: API_URL,
 })
--- src/config.tsx
+++ src/config.tsx
@@ -1,9 +1,8 @@
 import axiosClient from "axios";
 // TEST
-//  export const API_ROUTE = "http://localhost:9141";
-export const API_ROUTE = "https://apps.mediabox.bi:9141";
+//export const API_ROUTE = "https://apps.mediabox.bi:9141";
 
-export const API_URL = API_ROUTE;
+export const API_URL = process.env.REACT_APP_API_URL;
 export const axios = axiosClient.create({
   baseURL: API_URL,
 })
